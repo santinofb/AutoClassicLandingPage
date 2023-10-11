90de9078acc873baf63477b2e90e942449bc0c82
T1T098 <santinofabi98@gmail.com>
2023-10-10 22:29:16 -0300

    optimizadores-section y logos-brand remove

--- js/script.js
+++ js/script.js
@@ -102,3 +102,16 @@ desmarcar2.addEventListener("click", () => {
 desmarcar3.addEventListener("click", () => {
   checkbox.checked = false;
 });
+
+var btnAbrirPopup2 = document.getElementById('btn-abrir-popup2'),
+    overlay2 = document.getElementById('overlay2'),
+    popup2 = document.getElementById('popup2'),
+    btnCerrarPopup2 = document.getElementById('btn-cerrar-popup2');
+btnAbrirPopup2.addEventListener('click', function(){
+  overlay2.classList.add('active');
+  popup2.classList.add('active');
+});
+btnCerrarPopup2.addEventListener('click', function(){
+  overlay2.classList.remove('active');
+  popup2.classList.remove('active');
+});
