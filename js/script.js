1b663bc09d435e99af67ca24dc37f4b6b8fdcf32
T1T098 <santinofabi98@gmail.com>
2024-02-12 23:43:28 -0300

    format document

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 const evento = document.getElementById('send')
-const enviarFormulario =() => {
-        let nombre = document.getElementById('nombre').value;
-        let asunto = document.getElementById('asunto').value;
-        let mensaje = document.getElementById('mensaje').value;
-        let numero= 543412095624;
-var win= window.open(`https://wa.me/${numero}?text=Hola,%20mi%20nombre%20es%20${nombre}.%20Asunto:%20${asunto}.%20${mensaje}`,'_blank');
+const enviarFormulario = () => {
+  let nombre = document.getElementById('nombre').value;
+  let asunto = document.getElementById('asunto').value;
+  let mensaje = document.getElementById('mensaje').value;
+  let numero = 543412095624;
+  var win = window.open(`https://wa.me/${numero}?text=Hola,%20mi%20nombre%20es%20${nombre}.%20Asunto:%20${asunto}.%20${mensaje}`, '_blank');
 }
 evento.addEventListener('click', enviarFormulario)
 
@@ -12,33 +12,33 @@ const nombre = document.getElementById("nombre");
 const asunto = document.getElementById("asunto");
 const mensaje = document.getElementById("mensaje");
 nombre.addEventListener("input", (e) => {
-  const {data} = e;
+  const { data } = e;
   if (nombre.value.length === 1 && data) {
     nombre.value = data.toUpperCase();
   }
 });
 asunto.addEventListener("input", (e) => {
-        const {data} = e;
-        if (asunto.value.length === 1 && data) {
-          asunto.value = data.toUpperCase();
-        }
+  const { data } = e;
+  if (asunto.value.length === 1 && data) {
+    asunto.value = data.toUpperCase();
+  }
 });
 mensaje.addEventListener("input", (e) => {
-        const {data} = e;
-        if (mensaje.value.length === 1 && data) {
-          mensaje.value = data.toUpperCase();
-        }
+  const { data } = e;
+  if (mensaje.value.length === 1 && data) {
+    mensaje.value = data.toUpperCase();
+  }
 });
 
 var btnAbrirPopup = document.getElementById('btn-abrir-popup'),
-    overlay = document.getElementById('overlay'),
-    popup = document.getElementById('popup'),
-    btnCerrarPopup = document.getElementById('btn-cerrar-popup');
-btnAbrirPopup.addEventListener('click', function(){
+  overlay = document.getElementById('overlay'),
+  popup = document.getElementById('popup'),
+  btnCerrarPopup = document.getElementById('btn-cerrar-popup');
+btnAbrirPopup.addEventListener('click', function () {
   overlay.classList.add('active');
   popup.classList.add('active');
 });
-btnCerrarPopup.addEventListener('click', function(){
+btnCerrarPopup.addEventListener('click', function () {
   overlay.classList.remove('active');
   popup.classList.remove('active');
 });
@@ -58,7 +58,7 @@ var swiper = new Swiper(".slide-content", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-  breakpoints:{
+  breakpoints: {
     0: {
       slidesPerView: 2,
     },
@@ -75,25 +75,25 @@ const btnSwitch = document.querySelector('#container');
 btnSwitch.addEventListener('click', () => {
   document.body.classList.toggle('dark');
   btnSwitch.classList.toggle('active');
-  if(document.body.classList.contains('dark')){
-		localStorage.setItem('dark-mode', 'true');
-	} else {
-		localStorage.setItem('dark-mode', 'false');
-	}
+  if (document.body.classList.contains('dark')) {
+    localStorage.setItem('dark-mode', 'true');
+  } else {
+    localStorage.setItem('dark-mode', 'false');
+  }
 });
-if(localStorage.getItem('dark-mode') === 'true'){
-	document.body.classList.add('dark');
-	btnSwitch.classList.add('active');
+if (localStorage.getItem('dark-mode') === 'true') {
+  document.body.classList.add('dark');
+  btnSwitch.classList.add('active');
 } else {
-	document.body.classList.remove('dark');
-	btnSwitch.classList.remove('active');
+  document.body.classList.remove('dark');
+  btnSwitch.classList.remove('active');
 };
 
 const checkbox = document.querySelector("#check"),
-      desmarcar = document.querySelector("#desmarcar"),
-      desmarcar2 = document.querySelector("#desmarcar2"),
-      desmarcar3 = document.querySelector("#desmarcar3"),
-      togglemenu = document.querySelector("#toggle-menu");
+  desmarcar = document.querySelector("#desmarcar"),
+  desmarcar2 = document.querySelector("#desmarcar2"),
+  desmarcar3 = document.querySelector("#desmarcar3"),
+  togglemenu = document.querySelector("#toggle-menu");
 desmarcar.addEventListener("click", () => {
   checkbox.checked = false;
 });
@@ -117,16 +117,16 @@ desmarcar3.addEventListener("click", () => {
 });
 
 var btnAbrirPopup2 = document.getElementById('btn-abrir-popup2'),
-    overlay2 = document.getElementById('overlay2'),
-    popup2 = document.getElementById('popup2'),
-    btnCerrarPopup2 = document.getElementById('btn-cerrar-popup2'),
-    iframe = document.getElementById('iframe');
-btnAbrirPopup2.addEventListener('click', function(){
+  overlay2 = document.getElementById('overlay2'),
+  popup2 = document.getElementById('popup2'),
+  btnCerrarPopup2 = document.getElementById('btn-cerrar-popup2'),
+  iframe = document.getElementById('iframe');
+btnAbrirPopup2.addEventListener('click', function () {
   overlay2.classList.add('active');
   popup2.classList.add('active');
   iframe.setAttribute('src', 'https://www.youtube.com/embed/yyS5th5ctg0?si=vmyUECXpmPVXwhtn');
 });
-btnCerrarPopup2.addEventListener('click', function(){
+btnCerrarPopup2.addEventListener('click', function () {
   overlay2.classList.remove('active');
   popup2.classList.remove('active');
   iframe.removeAttribute('src');
